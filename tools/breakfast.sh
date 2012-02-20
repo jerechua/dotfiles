5022c1f71a75ec31a091df1178b359f713b776c3
Jeff Jewiss <jeffjewiss@gmail.com>
2012-02-19 19:34:39 -0500

    Adds simple version of installing Breakfast

--- tools/breakfast.sh
+++ tools/breakfast.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 cd &&
-if [ -d .middleman ]
+if [ -d ~/.middleman ]
 then
 	# cd into folder and pull
-	echo 'middleman folder exists'
+	echo "Middleman template folder exists."
+	if [ -d ~/.middleman/breakfast ]
+		echo "Breakfast has been previously installed, updating..."
+		cd ~/.middleman/breakfast && git pull origin master
+		# echo "The Breakfast template has been successfully installed."
+	fi
 else
-	# clone breakfast and cd into
-	echo 'no mm :('
+	# create the middleman template folder
+	echo "First we need to create the Middleman template folder."
+	mkdir ~/.middleman && cd ~/.middleman
+	echo "Installing Breakfast..."
+	git clone git://github.com/jeffjewiss/breakfast
 fi
 
-# [ -d '.files' ] || git clone git://github.com/jeffjewiss/dotfiles.git .files &&
-
-# current_path=`pwd`
-# ( cd && cd .files && git pull origin master )
-# ~/.files/suit_up.sh
-# cd "$current_path"
+echo "You can now start a Breakfast project with the command:"
+echo "$ middleman init -t breakfast"
